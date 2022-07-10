1d731b8695ea323af5b068d434af7193e0c6f52e
pedroanicetoo <pedro_madri@hotmail.com>
2022-07-10 13:37:23 -0300

    setup spec files and updte schema.db

---
+++ spec/test_helper.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+class FormFail
+
+    def register; end
+  
+    def valid?
+      false
+    end
+  
+    def errors
+      ActiveModel::Errors.new(
+        [ActiveModel::Error.new("doesn't matter", "doesn't matter")]
+      )
+    end
+  
+  end
+  
+  def valid_cpf
+    Faker::CPF.numeric
+  end
+  
+  def valid_cnpj
+    Faker::CNPJ.numeric
+  end
+  
+  def random_number(var = 100_000)
+    if var != 100_000
+      1 + SecureRandom.random_number(var)
+    else
+      1 + SecureRandom.random_number(var) + Time.now.to_i
+    end
+  end
+  
+  def random_string(size = 10)
+    SecureRandom.alphanumeric[0...size]
+  end
+  
+  def valid_email
+    "#{random_string}@#{random_string}.com"
+  end
+
+  def valid_uf
+    %w[RO AC AM RR PA AP TO MA PI CE RN PB PE AL SE BA MG ES RJ SP PR SC RS MS MT GO DF]
+  end
